0f857fe5c17e2d3f554fcc781c8e9ca842c1c0f7
Dyego Lima <dyegodll18@gmail.com>
2024-12-10 11:54:54 -0300

    first commit

---
+++ ApiKanyeThoughts/Models/KanyeQuote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ApiKanyeThoughts.Models
+{
+
+    internal class KanyeQuote
+    {
+        [JsonPropertyName("quote")]
+        public string? Quote { get; set; }
+    }
+}
---
+++ ApiKanyeThoughts/Program.cs
@@ -0,0 +1,34 @@
+﻿using ApiKanyeThoughts.Models;
+using System.Linq;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    //List<string> phrases = new List<string>();
+    var uniqueQuotes = new HashSet<string>();
+    for (int i = 0; i < 50; i++)
+    {
+        try
+        {
+            string json = await client.GetStringAsync("https://api.kanye.rest/");
+            var kanyeQuotes = JsonSerializer.Deserialize<KanyeQuote>(json)!;
+
+            if (kanyeQuotes != null && !uniqueQuotes.Contains(kanyeQuotes.Quote))
+            {
+                uniqueQuotes.Add(kanyeQuotes.Quote);
+                Console.WriteLine($"♦ {kanyeQuotes.Quote} ♦\n");
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
+    Console.WriteLine($"◘ Total quotes at this moment: {uniqueQuotes.Count} ◘");
+}
